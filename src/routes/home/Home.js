4e3f487227ef2c3497e32c792e011006aef00609
kevinchau321 <kevinchau321@berkeley.edu>
2017-02-21 18:00:28 -0800

    added eq-3 knobs

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -8,6 +8,7 @@
  */
 import React from 'react';
 import Slider from 'rc-slider';
+import Knob from 'react-canvas-knob';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 
@@ -15,32 +16,86 @@ const style = { float: 'left', width: 75, height: 400, marginBottom: 100 };
 const parentStyle = { overflow: 'hidden' };
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hi_value: 63, mid_value: 63, lo_value: 63 };
+  }
+  handleHIChange = (newValue) => {
+    this.setState({ hi_value: newValue });
+  };
+  handleMIDChange = (newValue) => {
+    this.setState({ mid_value: newValue });
+  };
+  handleLOChange = (newValue) => {
+    this.setState({ lo_value: newValue });
+  };
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
           <link rel="stylesheet" href="stylesheets/rc-slider.css" />
-          <div>
+          <div className="Title">
             <h1>TReactr</h1>
             <p> TReactr is a javascript digital DJ mixing application built for
             the modern web, based on the legendary UI from Native Instruments&#39; Traktor. </p>
           </div>
-          <div style={parentStyle}>
+          <div className="sliders" style={parentStyle}>
             <div style={style}>
-              <p> Deck A </p>
+              <p>Deck A</p>
               <Slider vertical max={127} />
             </div>
             <div style={style}>
-              <p> Deck B </p>
+              <p>Deck B</p>
               <Slider vertical max={127} />
             </div>
             <div style={style}>
-              <p> Deck C </p>
+              <p>Deck C</p>
               <Slider vertical max={127} />
             </div>
             <div style={style}>
-              <p> Deck D </p>
+              <p>Deck D</p>
               <Slider vertical max={127} />
+            </div>
+          </div>
+          <div className="knobs">
+            <div>
+              <p>HI</p>
+              <Knob
+                value={this.state.hi_value}
+                onChange={this.handleHIChange}
+                angleOffset={-150}
+                angleArc={300}
+                max={127}
+                thickness={0.25}
+                width={36}
+                disableTextInput
+              />
+            </div>
+            <div>
+              <p>MID</p>
+              <Knob
+                value={this.state.mid_value}
+                onChange={this.handleMIDChange}
+                angleOffset={-150}
+                angleArc={300}
+                max={127}
+                thickness={0.25}
+                width={36}
+                disableTextInput
+              />
+            </div>
+            <div>
+              <p>LO</p>
+              <Knob
+                value={this.state.lo_value}
+                onChange={this.handleLOChange}
+                angleOffset={-150}
+                angleArc={300}
+                max={127}
+                thickness={0.25}
+                width={36}
+                disableTextInput
+              />
             </div>
           </div>
         </div>
