1de0708f1d54b72afc2b8aa20e2dd825da168474
kevinchau321 <kevinchau321@berkeley.edu>
2017-04-17 01:13:52 -0700

    creating audio source node

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -18,7 +18,39 @@ class Deck extends React.Component {
   };
 
   componentDidMount() {
-    console.log('componentDidMount() called after render()');
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var myAudio = document.querySelector('audio');
+    console.log(myAudio);
+
+    // Create a MediaElementAudioSourceNode
+    // Feed the HTMLMediaElement into it
+    // var source = audioCtx.createMediaElementSource(myAudio);
+
+    // Create a gain node
+    var gainNode = audioCtx.createGain();
+    //
+    // // Create variables to store mouse pointer Y coordinate
+    // // and HEIGHT of screen
+    // var CurY;
+    // var HEIGHT = window.innerHeight;
+    //
+    // // Get new mouse pointer coordinates when mouse is moved
+    // // then set new gain value
+    //
+    // document.onmousemove = updatePage;
+    //
+    // function updatePage(e) {
+    //   CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+    //
+    //   gainNode.gain.value = CurY/HEIGHT;
+    // }
+    //
+    //
+    // // connect the AudioBufferSourceNode to the gainNode
+    // // and the gainNode to the destination, so we can play the
+    // // music and adjust the volume using the mouse cursor
+    // source.connect(gainNode);
+    // gainNode.connect(audioCtx.destination);
   }
 
   setVolume = (e) => {
@@ -26,7 +58,6 @@ class Deck extends React.Component {
   }
 
   render() {
-    console.log('render() called before componentDidMount');
     return (
       <div className={s.container}>
         <ReactPlayer
