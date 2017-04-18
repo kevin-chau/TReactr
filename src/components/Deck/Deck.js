de21497a9ac64fcd19322f805cc29b996b1ab9be
kevinchau321 <kevinchau321@berkeley.edu>
2017-04-18 00:11:02 -0500

    source node created, connected to gain node, playback with CORS enabled

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -9,49 +9,37 @@ class Deck extends React.Component {
     url: PropTypes.string,
     playing: PropTypes.bool,
     volume: PropTypes.number,
+    name: PropTypes.string,
   };
 
   static defaultProps = {
     url: '',
     playing: false,
     volume: 0,
+    name: '',
   };
 
   componentDidMount() {
-    if (this.props.name === "DeckD"){
+    if (this.props.name === 'DeckD') {
       const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
       const myAudio = document.querySelector('audio');
-      console.log(myAudio);
+
+      // set to anonymous for CORS
+      myAudio.crossOrigin = 'anonymous';
 
       // Create a MediaElementAudioSourceNode
       // Feed the HTMLMediaElement into it
-      var source = audioCtx.createMediaElementSource(myAudio);
+      const source = audioCtx.createMediaElementSource(myAudio);
 
       // Create a gain node
       const gainNode = audioCtx.createGain();
-      //
-      // // Create variables to store mouse pointer Y coordinate
-      // // and HEIGHT of screen
-      // var CurY;
-      // var HEIGHT = window.innerHeight;
-      //
-      // // Get new mouse pointer coordinates when mouse is moved
-      // // then set new gain value
-      //
-      // document.onmousemove = updatePage;
-      //
-      // function updatePage(e) {
-      //   CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-      //
-      //   gainNode.gain.value = CurY/HEIGHT;
-      // }
-      //
-      //
-      // // connect the AudioBufferSourceNode to the gainNode
-      // // and the gainNode to the destination, so we can play the
-      // // music and adjust the volume using the mouse cursor
-      // source.connect(gainNode);
-      // gainNode.connect(audioCtx.destination);
+      gainNode.gain.value = 1;
+
+      // connect the AudioBufferSourceNode to the gainNode
+      // and the gainNode to the destination, so we can play the
+      // music and adjust the volume using the mouse cursor
+      source.connect(gainNode);
+      gainNode.connect(audioCtx.destination);
     }
   }
 
