2f2e7e6d8f555dcfeff4334dea914af756910fe7
kevinchau321 <kevinchau321@berkeley.edu>
2017-04-18 00:01:30 -0500

    isolate componentDidMount for Deck D (soundcloud deck) only

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -18,39 +18,41 @@ class Deck extends React.Component {
   };
 
   componentDidMount() {
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var myAudio = document.querySelector('audio');
-    console.log(myAudio);
+    if (this.props.name === "DeckD"){
+      const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+      const myAudio = document.querySelector('audio');
+      console.log(myAudio);
 
-    // Create a MediaElementAudioSourceNode
-    // Feed the HTMLMediaElement into it
-    // var source = audioCtx.createMediaElementSource(myAudio);
+      // Create a MediaElementAudioSourceNode
+      // Feed the HTMLMediaElement into it
+      var source = audioCtx.createMediaElementSource(myAudio);
 
-    // Create a gain node
-    var gainNode = audioCtx.createGain();
-    //
-    // // Create variables to store mouse pointer Y coordinate
-    // // and HEIGHT of screen
-    // var CurY;
-    // var HEIGHT = window.innerHeight;
-    //
-    // // Get new mouse pointer coordinates when mouse is moved
-    // // then set new gain value
-    //
-    // document.onmousemove = updatePage;
-    //
-    // function updatePage(e) {
-    //   CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-    //
-    //   gainNode.gain.value = CurY/HEIGHT;
-    // }
-    //
-    //
-    // // connect the AudioBufferSourceNode to the gainNode
-    // // and the gainNode to the destination, so we can play the
-    // // music and adjust the volume using the mouse cursor
-    // source.connect(gainNode);
-    // gainNode.connect(audioCtx.destination);
+      // Create a gain node
+      const gainNode = audioCtx.createGain();
+      //
+      // // Create variables to store mouse pointer Y coordinate
+      // // and HEIGHT of screen
+      // var CurY;
+      // var HEIGHT = window.innerHeight;
+      //
+      // // Get new mouse pointer coordinates when mouse is moved
+      // // then set new gain value
+      //
+      // document.onmousemove = updatePage;
+      //
+      // function updatePage(e) {
+      //   CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+      //
+      //   gainNode.gain.value = CurY/HEIGHT;
+      // }
+      //
+      //
+      // // connect the AudioBufferSourceNode to the gainNode
+      // // and the gainNode to the destination, so we can play the
+      // // music and adjust the volume using the mouse cursor
+      // source.connect(gainNode);
+      // gainNode.connect(audioCtx.destination);
+    }
   }
 
   setVolume = (e) => {
