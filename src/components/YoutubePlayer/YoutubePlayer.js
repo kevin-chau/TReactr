881e60ac16b9a69c9d130439d8712846b283312a
kevinchau321 <kevinchau321@berkeley.edu>
2017-04-24 02:17:54 -0500

    more code clean up

--- src/components/YoutubePlayer/YoutubePlayer.js
+++ src/components/YoutubePlayer/YoutubePlayer.js
@@ -15,83 +15,82 @@ import s from './YoutubePlayer.css';
 
 class YoutubePlayer extends React.Component {
   render() {
-    return(
+    return (
       <video controls="true">
         <source src="https://www.youtube.com/watch?v=FM7MFYoylVs" type="video/mp4" />
       </video>
-    )
+    );
   }
   componentDidMount() {
     function defer(method) {
-      if (true)
-          method();
-      else
-          setTimeout(function() {
-              defer(method)
-          }, 50);
+      if (true) { method(); } else {
+        setTimeout(() => {
+          defer(method);
+        }, 50);
+      }
     }
 
-    var videos = document.querySelectorAll("video");
+    const videos = document.querySelectorAll('video');
 
-    console.log("GOT TO HERE BITCH!");
+    console.log('GOT TO HERE BITCH!');
 
-    var mp4downloadlink;
+    let mp4downloadlink;
 
-    var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+    const cors_api_url = 'https://cors-anywhere.herokuapp.com/';
 
-    var done = 0;
+    let done = 0;
 
-    defer(function() {
-        function doCORSRequest(options, printResult) {
-            var x = new XMLHttpRequest();
-            var src = videos[0].querySelector('source').src;
-            console.log("video url is: " + src);
+    defer(() => {
+      function doCORSRequest(options, printResult) {
+        const x = new XMLHttpRequest();
+        const src = videos[0].querySelector('source').src;
+        console.log(`video url is: ${src}`);
 
-            var id;
+        let id;
 
-            if (src) {
-                var isYoutube = src && src.match(/(?:youtu|youtube)(?:\.com|\.be)\/([\w\W]+)/i);
-                console.log("isYoutube: " + isYoutube);
-                if (isYoutube) {
-                    id = isYoutube[1].match(/watch\?v=|[\w\W]+/gi);
-                    id = (id.length > 1) ? id.splice(1) : id;
-                    id = id.toString();
-                }
-            }
-
-            x.open('GET', cors_api_url + 'youtubeinmp4.com/redirect.php?video=' + id);
-            x.onload = x.onerror = function() {
-                var content = $(x.responseText).find('.downloadButtons');
-                console.log("mp4 download link (1) is : " + $(content[0]).attr('href'));
-                mp4downloadlink = $(content[0]).attr('href');
-                done = 1;
-            };
-            x.send(options.data);
+        if (src) {
+          const isYoutube = src && src.match(/(?:youtu|youtube)(?:\.com|\.be)\/([\w\W]+)/i);
+          console.log(`isYoutube: ${isYoutube}`);
+          if (isYoutube) {
+            id = isYoutube[1].match(/watch\?v=|[\w\W]+/gi);
+            id = (id.length > 1) ? id.splice(1) : id;
+            id = id.toString();
+          }
         }
-        (function() {
-            doCORSRequest({
-                method: 'GET',
-            });
-        })();
+
+        x.open('GET', `${cors_api_url}youtubeinmp4.com/redirect.php?video=${id}`);
+        x.onload = x.onerror = function () {
+          const content = $(x.responseText).find('.downloadButtons');
+          console.log(`mp4 download link (1) is : ${$(content[0]).attr('href')}`);
+          mp4downloadlink = $(content[0]).attr('href');
+          done = 1;
+        };
+        x.send(options.data);
+      }
+      (function () {
+        doCORSRequest({
+          method: 'GET',
+        });
+      }());
     });
 
     function checkFlag() {
-        if (done === 0) {
-            window.setTimeout(checkFlag, 100); /* this checks the flag every 100 milliseconds*/
-        } else {
+      if (done === 0) {
+        window.setTimeout(checkFlag, 100); /* this checks the flag every 100 milliseconds*/
+      } else {
             /* do something*/
-            loadvideo();
-        }
+        loadvideo();
+      }
     }
     checkFlag();
 
     function loadvideo() {
-        console.log("LOADING YOUTUBE HTML5 VIDEO");
-        for (var i = 0, l = videos.length; i < l; i++) {
-            var video = videos[i];
-            var mp4url = "http://www.youtubeinmp4.com/";
-            video.src = mp4url + mp4downloadlink;
-        }
+      console.log('LOADING YOUTUBE HTML5 VIDEO');
+      for (let i = 0, l = videos.length; i < l; i++) {
+        const video = videos[i];
+        const mp4url = 'http://www.youtubeinmp4.com/';
+        video.src = mp4url + mp4downloadlink;
+      }
     }
   }
 }
